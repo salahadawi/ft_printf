bb79c41923fbcbce025809d42941a80d1eb6cfc8
Salah Adawi <hahli144@hotmail.com>
2019-11-10 18:23:23 +0200

    implement flag checking

--- ft_printf.c
+++ ft_printf.c
@@ -13,8 +13,66 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include "libft/libft.h"
+
+int	check_flag(char *str);
 
 int	ft_printf(const char *format, ...)
 {
+  va_list args;
+  int arg;
 
+  va_start(args, format);
+  while (arg != 0)
+    {
+      arg = va_arg(args, int);
+      ft_putnbr(arg);
+    }
+  return (0);
 }
+
+char **sort_flags(char *format)
+{
+  char **flags;
+
+  if (!arr = (char**)malloc(sizeof(arr) * flags_amount
+  return (0);
+}
+
+int	flags_amount(char *str)
+{
+  int i;
+
+  i = 0;
+  while (*str)
+    {
+      if (*str == '%')
+	if (*(str + 1) == '%')
+	  str++;
+	else if (*(str + 1) != '\0')
+	  i++;
+      str++;
+    }
+  return (i);
+}
+
+int	check_flag(char *str)
+{
+  int i;
+
+  i = 0;
+  ft_putnbr(flags_amount(str));
+  return (0);
+  
+}
+
+int	main(int argc, char **argv)
+{
+  (void)argc;
+  check_flag(argv[1]);
+  ft_printf(argv[1], 5, 6, 7);
+  return (0);
+}
+
+//count each % in format, take in to memory all flags, then check va_arg for content
+//or, read straight from format, when % is encountered check if valid flag, then find content from va_arg
