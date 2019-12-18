04cee296c99c613542d4ef34ffc118d3de3241e8
Salah Adawi <53702890+Hahli144@users.noreply.github.com>
2019-12-18 14:24:03 +0200

    move char functions to handle_char.c

--- ft_printf.c
+++ ft_printf.c
@@ -60,28 +60,3 @@ int		handle_output(char **output, char *flag)
 	free(*output);
 	return (len);
 }
-
-int		handle_char_output(char **output, char *flag)
-{
-	int width;
-	int i;
-
-	i = 0;
-	width = 1;
-	while (ft_strchr("#0-+ ", flag[i]))
-		i++;
-	if (ft_isdigit(flag[i]))
-		width = ft_atoi(flag + i);
-	i = 0;
-	while (width > i)
-		ft_putchar((*output)[i++]);
-	return (width);
-}
-
-int		handle_char_width(char **output, char *flag, int width)
-{
-	if (ft_strchr(flag, 'c'))
-		if (!(**output) && width > 1)
-			return (--width);
-	return (width);
-}
